3448e74f96b2abc3c939c64a5e4c3f670ee12738
yurii vasylchenko <yurri.vasylenko@everlution.sk>
2024-03-09 17:39:43 +0100

    #sqlAlchemySchema-1

--- src/components/DrawingItem.tsx
+++ src/components/DrawingItem.tsx
@@ -1,15 +1,37 @@
 import { Text, View, Image } from "react-native";
 import prettifyNames from "../helpers/prettifyNames";
-import { styles } from "../styles/styles";
-import { Children } from "react";
-import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
 import { useTranslation } from "react-i18next";
+import i18next from "i18next";
 
-export function DrawingItem({
-  item,
-}: {
-  item: { id: string; name: string; image: string };
-}) {
+enum ElementType {
+  GRIPPER = "Gripper",
+  LINE_EQUIPMENT = "Line Equipment",
+  FITTINGS_N_TUBES = "Fittings and Tubes",
+  FLOW_CONTROL_EQUIPMENT = "Flow Control Equipment",
+  MECHANICAL_N_AIR_OPERATED_VALVES = "Mechanical and Air Operated Valves",
+  PNEUMATIC_ACTUATORS = "Pneumatic Actuators",
+  PRESSURE_N_VACUUM_SWITCHES = "Pressure and Vacuum Switches",
+  PROCESS_TECHNOLOGY = "Process Technology",
+  ROTARY_ACTUATOR = "Rotary actuator",
+  SAFETY_PRESSURE_RELEASE_VALVES = "Safety pressure release valves",
+  SOLENOID_VALVES = "Solenoid Valves",
+  VACUUM_EQUIPMENT = "Vacuum Equipment",
+  OTHER = "Other",
+}
+
+type Drawing = {
+  id: string;
+  name_en: string;
+  name_sk: string;
+  name_ua: string;
+  image: string;
+  type: ElementType;
+  description_en: string;
+  description_sk: string;
+  description_ua: string;
+};
+
+export function DrawingItem({ item }: { item: Drawing }) {
   const { t } = useTranslation();
   return (
     <View
@@ -29,7 +51,9 @@ export function DrawingItem({
           // backgroundColor: "pink",
         }}
       >
-        {prettifyNames(item.name)}
+        {i18next.language === "en" && prettifyNames(item.name_en)}
+        {i18next.language === "sk" && prettifyNames(item.name_sk)}
+        {i18next.language === "ua" && prettifyNames(item.name_ua)}
       </Text>
       <View
         style={{
@@ -41,12 +65,21 @@ export function DrawingItem({
         }}
       >
         <Image
-          style={{ width: 340, height: 180 }}
+          style={{
+            width: 340,
+            height: 180,
+            resizeMode: "contain",
+            borderColor: "black",
+            backgroundColor: "white",
+            borderWidth: 0.2,
+            borderRadius: 10,
+          }}
           source={{
             uri: `data:image/jpeg;base64,${item.image}`,
           }}
         />
       </View>
+      <Text>{"type:\n" + item.type}</Text>
       <Text
         style={{
           marginLeft: 30,
@@ -54,9 +87,15 @@ export function DrawingItem({
           marginTop: 5,
           fontSize: 15,
           fontWeight: "500",
+          textAlign: "justify",
         }}
       >
-        {t("describtion") + ": "}
+        {i18next.language === "en" &&
+          t("describtion") + ":\n" + item.description_en}
+        {i18next.language === "sk" &&
+          t("describtion") + ":\n" + item.description_sk}
+        {i18next.language === "ua" &&
+          t("describtion") + ":\n" + item.description_ua}
       </Text>
     </View>
   );
